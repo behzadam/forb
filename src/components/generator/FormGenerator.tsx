5ed4c54abd096e9db8548731b32795b63d2f8125
Behzad AM <behzad.am@gmail.com>
2022-01-03 00:17:23 +0330

    feat: update form style

--- src/components/generator/FormGenerator.tsx
+++ src/components/generator/FormGenerator.tsx
@@ -16,30 +16,41 @@ const FormGenerator = ({ formData }: FormProps): ReactElement => {
 
   if (isLoading) return <Spinner />;
   return (
-    <div className="container mx-auto">
-      <Formik
-        initialValues={formValues}
-        enableReinitialize={true}
-        onSubmit={async (values) => onSubmit(values)}
-      >
-        <Form>
-          {fields.map((field: FieldType) => {
-            return (
-              <Field
-                key={field.uid}
-                type={field.type}
-                name={field.uid}
-                id={field.uid}
-                className="block mt-4 border border-gray-500"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                  fieldChanged(field.uid, e.target.value);
-                }}
-              />
-            );
-          })}
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
+    <div className="max-w-sm mx-auto">
+      <h1 className="mb-4 font-mono text-base font-bold">
+        Forb / Form Generator
+      </h1>
+      <div className="p-6 bg-white border rounded-md shadow-sm border-gray-50">
+        <Formik
+          initialValues={formValues}
+          enableReinitialize={true}
+          onSubmit={async (values) => onSubmit(values)}
+        >
+          <Form>
+            {fields.map((field: FieldType) => {
+              return (
+                <div className="mb-6" key={field.uid}>
+                  <Field
+                    type={field.type}
+                    name={field.uid}
+                    id={field.uid}
+                    className="text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                      fieldChanged(field.uid, e.target.value);
+                    }}
+                  />
+                </div>
+              );
+            })}
+            <button
+              type="button"
+              className="text-white bg-indigo-600 focus:ring-4 focus:ring-indigo-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
+            >
+              Submit
+            </button>
+          </Form>
+        </Formik>
+      </div>
     </div>
   );
 };
