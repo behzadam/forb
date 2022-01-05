15c5925dc102e4152443dfb85c87a52ed4c97b21
Behzad AM <behzad.am@gmail.com>
2022-01-05 21:41:00 +0330

    test: add useFormGenerator hook test

--- src/components/generator/useFormGenerator.test.tsx
+++ src/components/generator/useFormGenerator.test.tsx
@@ -1,1 +1,12 @@
-describe('useFormGenerator', () => {});
+import { renderHook } from '@testing-library/react-hooks';
+
+import { formData } from './FormData';
+import useFormGenerator from './useFormGenerator';
+
+describe('useFormGenerator', () => {
+  test(`Default value of will be zero`, () => {
+    const { result } = renderHook(() => useFormGenerator(formData));
+    const size = result.current.fields.length;
+    expect(size).toBeGreaterThan(0);
+  });
+});
