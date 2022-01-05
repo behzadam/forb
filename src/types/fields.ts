3b19df1aecc901c855f01fff79c129c45bc0e215
Behzad AM <behzad.am@gmail.com>
2022-01-05 06:34:27 +0330

    feat: add radio options

--- src/types/fields.ts
+++ src/types/fields.ts
@@ -30,15 +30,23 @@ export enum ActionsType {
 
 export type FieldValues = Record<string, any>;
 
+export type OptionType = {
+  label: string;
+  value: string;
+  checked: boolean;
+  uid: string;
+};
+
 export type FieldType = {
   uid: string;
   label: string;
   type: string;
-  value: any;
+  value?: any;
   condition?: string;
   logic?: {
     if: string;
     conditions: Conditions[];
   };
+  options?: OptionType[];
   onChange(uid: string, value: string): void;
 };
