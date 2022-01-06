5e457401bf1e9d9547cabe72dec5dad8aa067c61
Behzad AM <behzad.am@gmail.com>
2022-01-07 01:11:31 +0330

    test: update test

--- src/utils/operator.test.ts
+++ src/utils/operator.test.ts
@@ -1,6 +1,12 @@
-import { isEmpty } from './operator';
+import { ConditionsType } from '../types';
+import { isEmpty, checkCondition } from './operator';
 
 describe('operator', () => {
+  test('should call checkCondition', () => {
+    const result = checkCondition(ConditionsType.EqualTo, '1', '1');
+    expect(result).toBe(true);
+  });
+
   test('should return true on empty param', () => {
     expect(isEmpty(null)).toBe(true);
     expect(isEmpty(undefined)).toBe(true);
