06b0ecbdd59a6ebbf22a74228f283089831d0a0f
Behzad AM <behzad.am@gmail.com>
2022-01-06 17:57:23 +0330

    test: update tests

--- src/components/generator/useFormGenerator.test.tsx
+++ src/components/generator/useFormGenerator.test.tsx
@@ -1,12 +1,37 @@
-import { renderHook } from '@testing-library/react-hooks';
+import { act, renderHook } from '@testing-library/react-hooks';
 
-import { formData } from './FormData';
 import useFormGenerator from './useFormGenerator';
 
+let formData: any[] = [];
+beforeAll(() => {
+  formData = [{ uid: 'f61233e8-565e', value: 1 }];
+});
+
 describe('useFormGenerator', () => {
-  test(`Default value of will be zero`, () => {
+  test(`should sets fields state`, () => {
     const { result } = renderHook(() => useFormGenerator(formData));
-    const size = result.current.fields.length;
-    expect(size).toBeGreaterThan(0);
+    expect(result.current.fields.length).toBeGreaterThan(0);
+  });
+
+  test(`should initials key/value object list`, () => {
+    const { result } = renderHook(() => useFormGenerator(formData));
+    const { uid } = result.current.fields[0];
+    expect(result.current.formValues).toHaveProperty(uid);
+  });
+
+  test(`should update form value by uid`, () => {
+    const { result } = renderHook(() => useFormGenerator(formData));
+    const firstItem = formData[0];
+    const value = 2;
+
+    act(() => {
+      result.current.fieldChanged(firstItem.uid, value);
+    });
+
+    expect(result.current.formValues).toEqual(
+      expect.objectContaining({
+        [firstItem.uid]: value,
+      })
+    );
   });
 });
