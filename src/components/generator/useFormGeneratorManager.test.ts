dfa16d09ac8bbee6552318f7595b5dd522636e0e
Behzad AM <behzad.am@gmail.com>
2022-01-07 21:51:11 +0330

    test: update tests

--- src/components/generator/useFormGeneratorManager.test.ts
+++ src/components/generator/useFormGeneratorManager.test.ts
@@ -1,22 +1,13 @@
 import { ConditionsType } from '../../types';
-import { isEmpty, ifMeetsCondition } from './useFormGeneratorManager';
+import { ifMeetsCondition } from './useFormGeneratorManager';
 
-describe('operator', () => {
-  test('should call checkCondition', () => {
-    const result = ifMeetsCondition(ConditionsType.EqualTo, '1', '1');
+describe(`Operator`, () => {
+  test(`should ${ConditionsType.EqualTo} return true`, () => {
+    let result = ifMeetsCondition(ConditionsType.EqualTo, '1', '1');
     expect(result).toBe(true);
-  });
-
-  test('should return true on empty param', () => {
-    expect(isEmpty(null)).toBe(true);
-    expect(isEmpty(undefined)).toBe(true);
-    expect(isEmpty('')).toBe(true);
-    expect(isEmpty([])).toBe(true);
-    expect(isEmpty({})).toBe(true);
-  });
-
-  test('should return false on non-empty param', () => {
-    expect(isEmpty('1')).toBe(false);
-    expect(isEmpty([1])).toBe(false);
+    result = ifMeetsCondition(ConditionsType.EqualTo, 'Behzad', 'Behzad');
+    expect(result).toBe(true);
+    result = ifMeetsCondition(ConditionsType.EqualTo, 1, 1);
+    expect(result).toBe(true);
   });
 });
