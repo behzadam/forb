82eeef053ca610d8e89470ea0b2eb7df2329c30a
Behzad AM <behzad.am@gmail.com>
2022-01-09 00:14:27 +0330

    test: update tests

--- src/components/generator/FormGeneratorManager.test.ts
+++ src/components/generator/FormGeneratorManager.test.ts
@@ -1,5 +1,10 @@
 import { ConditionsType } from '../../types';
-import { ifMeetsCondition, isContains, isEmpty } from './FormGeneratorManager';
+import {
+  ifMeetsCondition,
+  isContains,
+  isEmpty,
+  isEquals,
+} from './FormGeneratorManager';
 
 describe('isEmpty', () => {
   test('should return true on empty values', () => {
@@ -57,6 +62,50 @@ describe('isContains', () => {
     const picked = null;
 
     const result = isContains(target, picked);
+    expect(result).toBe(false);
+  });
+});
+
+describe(`isEquals`, () => {
+  test(`should return true if strings are eual`, () => {
+    const target = 'hello';
+    const picked = 'hello';
+    const result = isEquals(target, picked);
+    expect(result).toBe(true);
+  });
+
+  test(`should return false if picked is an empty string`, () => {
+    const target = 'hello';
+    const picked = '';
+    const result = isEquals(target, picked);
+    expect(result).toBe(false);
+  });
+
+  test(`should return false if picked is null`, () => {
+    const target = 'hello';
+    const picked = null;
+    const result = isEquals(target, picked);
+    expect(result).toBe(false);
+  });
+
+  test(`should return false if strings are not equal`, () => {
+    const target = 'a';
+    const picked = 'b';
+    const result = isEquals(target, picked);
+    expect(result).toBe(false);
+  });
+
+  test(`should return true if non English strings are equal`, () => {
+    const target = 'سلام';
+    const picked = 'سلام';
+    const result = isEquals(target, picked);
+    expect(result).toBe(true);
+  });
+
+  test(`should return true if non English strings are not equal`, () => {
+    const target = 'سلام دنیا';
+    const picked = 'سلام';
+    const result = isEquals(target, picked);
     expect(result).toBe(false);
   });
 });
