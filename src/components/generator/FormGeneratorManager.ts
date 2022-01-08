f85fa206cdea917dd280579d69945a971c49c06a
Behzad AM <behzad.am@gmail.com>
2022-01-08 17:50:03 +0330

    feat: update isEquals

--- src/components/generator/FormGeneratorManager.ts
+++ src/components/generator/FormGeneratorManager.ts
@@ -19,9 +19,11 @@ function isContains(target: any, other: any): boolean {
   return target.some((item: string) => other.includes(item));
 }
 
-export const isEquals = (target: any, other: any): boolean => {
+function isEquals(target: string, other: string): boolean;
+function isEquals(target: number, other: number): boolean;
+function isEquals(target: string | number, other: string | number): boolean {
   return target === other;
-};
+}
 
 export const isGreaterThan = (target: number, other: number): boolean => {
   return target > other;
