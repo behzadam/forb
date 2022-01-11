7252e3120365f8f18a1a80d2741f934d2f3388e9
Behzad AM <behzad.am@gmail.com>
2022-01-11 16:09:05 +0330

    test: add custom field tests

---
+++ src/components/generator/Field.test.tsx
@@ -0,0 +1,6 @@
+describe('Field', () => {
+  test('should render component', () => {});
+  test('should update field value', () => {});
+  test('should call onChange callback', () => {});
+  test('should show / hide on the condition', () => {});
+});
