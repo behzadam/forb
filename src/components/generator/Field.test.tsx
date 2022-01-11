5beca5f708e55aa92c38f2ff7334c215516e2ab5
Behzad AM <behzad.am@gmail.com>
2022-01-11 23:56:48 +0330

    refactor: remove custom field

--- src/components/generator/Field.test.tsx
+++
@@ -1,6 +0,0 @@
-describe('Field', () => {
-  test('should render component', () => {});
-  test('should update field value', () => {});
-  test('should call onChange callback', () => {});
-  test('should show / hide on the condition', () => {});
-});
--- src/components/generator/Field.tsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-
-export const Field = (): JSX.Element => <div>Hello Field</div>;
