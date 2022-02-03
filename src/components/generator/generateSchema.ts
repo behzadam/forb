35b807903b0d97733a771912dde75ba2670c3748
Behzad AM <behzad.am@gmail.com>
2022-02-03 12:50:36 +0330

    chore: refactor returned schema

--- src/components/generator/generateSchema.ts
+++ src/components/generator/generateSchema.ts
@@ -11,20 +11,17 @@ type Config = {
 type Schema = { [uid: string]: any };
 
 export function generateSchema(schema: Schema, { uid, validation }: Config) {
-  console.log('Generating schema', schema, uid, validation);
-
   if (!validation?.type) return schema;
-
   let validator: { [type: string]: any } = (yup as any)[validation.type]();
   validation.conditions.forEach((condition) => {
     const { type, must } = condition;
     if (!validator[type]) {
       return;
     }
-    console.log(type, must);
     validator = validator[type](...must);
   });
-  // eslint-disable-next-line no-param-reassign
-  schema[uid] = validator;
-  return schema;
+  return {
+    ...schema,
+    [uid]: validator,
+  };
 }
