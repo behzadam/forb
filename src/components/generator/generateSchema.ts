70a3fe70df5c755f53cee5b39fad018eb27ba127
Behzad AM <behzad.am@gmail.com>
2022-02-04 01:31:58 +0330

    refactor: update generateSchema

--- src/components/generator/generateSchema.ts
+++ src/components/generator/generateSchema.ts
@@ -1,22 +1,24 @@
 import * as yup from 'yup';
 
-type Config = {
-  uid: any;
-  validation: {
-    type: string;
-    conditions: [{ type: string; must: [] }];
-  };
-};
-
 type Schema = { [uid: string]: any };
+type Config = { uid: string; validationType: string; validations: string[] };
 
-export function generateSchema(schema: Schema, { uid, validation }: Config) {
-  if (!validation?.type) return schema;
-  let validator: { [type: string]: any } = (yup as any)[validation.type]();
-  validation.conditions.forEach((condition) => {
-    const { type, must } = condition;
-    validator = validator[type](...must);
-    console.log(validator, must);
+export function generateSchema(
+  schema: Schema,
+  { uid, validationType, validations = [] }: Config
+) {
+  if (!(yup as any)[validationType]) {
+    return schema;
+  }
+  let validator: { [type: string]: any } = (yup as any)[validationType]();
+  validations.forEach((validation) => {
+    if (Array.isArray(validation)) {
+      validation?.forEach(() => {
+        const functionName = validation[0];
+        const validationParams = [...validation.slice(1)];
+        validator = validator[functionName](...validationParams);
+      });
+    }
   });
   return {
     ...schema,
