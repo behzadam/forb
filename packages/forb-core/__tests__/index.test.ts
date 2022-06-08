9ea9061681485b676829dcd0fc7efe4944c25631
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2022-06-09 01:11:35 +0430

    test: update generateFormValues test case

--- packages/forb-core/__tests__/index.test.ts
+++ packages/forb-core/__tests__/index.test.ts
@@ -1,11 +1,11 @@
 import { generateFormValues } from '../src/index'
 import formData from '../src/formData.json'
-import { FieldType } from '../src/types'
+// import { FieldType } from '../src/types'
 
 describe('Generate Form Values', () => {
   it('should generate key values form list with default values', () => {
-    // const data = formData
-    // const values = generateFormValues(data)
-    // console.log('values')
+    const data = formData
+    const values = generateFormValues(data)
+    console.log(values)
   })
 })
